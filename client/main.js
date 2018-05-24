3605f8328dec9c926242e7c5ce258972a7606b3c
harry <machengxue707@gmail.com>
2018-05-24 15:41:49 +0800

    change the template

--- client/main.js
+++ client/main.js
@@ -6,6 +6,23 @@ import './main.html';
 var borrowerVar = "eos";
 var lenderVar = "eos";
 
+document.addEventListener('scatterLoaded', scatterExtension => {
+  // Scatter will now be available from the window scope.
+  // At this stage the connection to Scatter from the application is 
+  // already encrypted. 
+  const scatter = window.scatter;
+  
+  // It is good practice to take this off the window once you have 
+  // a reference to it.
+  window.scatter = null;
+   
+  // If you want to require a specific version of Scatter
+  scatter.requireVersion(3.0);
+  
+  //...
+})
+
+
 //------------------borrower----------------------------------
 Template.borrower.onCreated(function helloOnCreated() {
   // counter starts at 0
