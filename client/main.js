c48a65bfac3b9d48bbe6920b77462a0bfc86c327
harry <machengxue707@gmail.com>
2018-05-24 14:09:48 +0800

    add the myapp project to the github

---
+++ client/main.js
@@ -0,0 +1,60 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+var borrowerVar = "eos";
+var lenderVar = "eos";
+
+//------------------borrower----------------------------------
+Template.borrower.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.change = new ReactiveVar(19);
+  
+});
+
+Template.borrower.helpers({
+  change() {
+    return Template.instance().change.get();
+  },
+});
+
+
+
+Template.borrower.events({
+  'submit form'(event) {
+  	event.preventDefault();
+    var form_hello = event.target.firstname.value;
+    //var form_select = event.target.elend.value;
+    console.log("wjat");
+    console.log(form_hello);
+  },
+});
+
+
+Template.borrower.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.change.set(instance.change.get() + 1);
+    // var form_hello = event.target.firstname.value;
+    // console.log(form_hello);
+  },
+});
+
+Template.borrower.events({
+  'change select'(event) {
+    event.preventDefault();
+   borrowerVar = event.target.value;
+    console.log("hello bor:" + borrowerVar);
+  },
+});
+
+//------------------lender----------------------------------
+Template.lender.events({
+  'change select'(event) {
+    event.preventDefault();
+   lenderVar = event.target.value;
+    console.log(lenderVar);
+  },
+});
+
