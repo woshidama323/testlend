ec50cb8b4f2a7a6f66a116e7d440c52574e78b61
harry <machengxue707@gmail.com>
2018-05-24 19:51:04 +0800

    add eosio which from npm

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import eos from 'eosjs'; 
+import { eosio } from 'eosjs'; 
 
 import './main.html';
 
