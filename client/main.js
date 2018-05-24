bb62c2dd49fb073f11faa9c9c1c4796e4333a3ee
harry <machengxue707@gmail.com>
2018-05-24 19:47:53 +0800

    change the template

--- client/main.js
+++ client/main.js
@@ -1,26 +1,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import eos from 'eosjs'; 
+
 import './main.html';
 
 var borrowerVar = "eos";
 var lenderVar = "eos";
-
-document.addEventListener('scatterLoaded', scatterExtension => {
-  // Scatter will now be available from the window scope.
-  // At this stage the connection to Scatter from the application is 
-  // already encrypted. 
-  const scatter = window.scatter;
-  
-  // It is good practice to take this off the window once you have 
-  // a reference to it.
-  window.scatter = null;
-   
-  // If you want to require a specific version of Scatter
-  scatter.requireVersion(3.0);
-  
-  //...
-})
 
 
 //------------------borrower----------------------------------
